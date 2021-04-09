4d62aa1d7d8f50596e89386125309555e2791cad
TranTaiDakLak <48401813+TranTaiDakLak@users.noreply.github.com>
2021-04-09 16:54:24 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "localhost";
+$servername = "ec2-23-21-229-200.compute-1.amazonaws.com";
 $username = "fasoxdddbmjmuu";
 $password = "829a1c6ab437a586dc620ffe279bd66f32d4f96acfc748493c174620c0cc7d47";
 $dbname = "d85gveusb00cai";
